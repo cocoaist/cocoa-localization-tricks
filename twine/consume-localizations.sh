e0f8e0b508fa453deda03773dd7fe767a5f9ac5d
Cemal Eker <cemaleker@gmail.com>
2016-02-29 15:22:10 +0200

    Implemented twine helper localization files

---
+++ twine/consume-localizations.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+source ${0%/*}/setup.sh
+
+find $SOURCE_DIR -name \*.m \
+	| xargs genstrings -o $LOCALIZATION_DIRECTORY/$DEVELOPMENT_LANGUAGE.lproj/
+
+echo "" > twine/strings
+
+echo "Consuming development language"
+twine consume-all-string-files \
+	$TWINE_FILE \
+	$LOCALIZATION_DIRECTORY/$DEVELOPMENT_LANGUAGE.lproj \
+ 	--developer-language $DEVELOPMENT_LANGUAGE \
+	--consume-all \
+	--consume-comments
+
+echo "Consuming additional languages"
+for i in ${OTHER_LANGUAGES[@]}; do 
+echo "Consuming " $i
+twine consume-all-string-files \
+	$TWINE_FILE \
+	$LOCALIZATION_DIRECTORY/$i.lproj
+done
